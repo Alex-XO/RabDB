f94a2ea2b3b05751e6bca7d0d5c03c846386a38c
Alex-XO <s2a5s0h6a@mail.ru>
2023-07-30 23:01:01 +0500

    Перенос команд в enum + добавлен обработчик ошибок

--- RabDB/Program.cs
+++ RabDB/Program.cs
@@ -1,18 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Data.SqlClient;
 
 namespace RabDB
 {
     class Program
     {
+        enum Commands
+        {
+            Create = 1,
+            FindByName = 2,
+            Edit = 3,
+            Delete = 4,
+            DeleteAll = 5,
+            Exit = 6
+        }
         static void Main(string[] args)
         {
             MyDatabase db = new MyDatabase();
             db.ConnectDB();
-            Console.WriteLine("Вы хотите создать, редактировать, найти или удалить пользователя/всех из БД?");
-            string answer = Console.ReadLine().ToLower();
 
-            ExecuteCommand(answer, db);
+            Commands? command = null;
+
+            while (command != Commands.Exit)
+            {
+                Console.WriteLine("Введите команду, чтобы продолжить: ");
+                var commands = Enum.GetValues(typeof(Commands));
+                foreach (var currentCommand in commands)
+                {
+                    Console.WriteLine($"{(int)currentCommand}.{currentCommand}");
+                }
+                
+                var answer = Console.ReadLine().ToLower();
+                
+                try
+                {
+                    command = Enum.Parse<Commands>(answer, true);
+                    ExecutingCommands(command, db);
+                } catch(Exception e)
+                {
+                    Console.WriteLine("Команда " + answer + " не существует");
+                }
+
+                
+            }
 
             db.ConnectionClose();
         }
@@ -23,47 +54,40 @@ namespace RabDB
             return Console.ReadLine();
         }
 
-        static void ExecuteCommand(string command, MyDatabase db)
+        static void ExecutingCommands(Commands? command, MyDatabase db)
         {
-            if (command == "создать")
+            switch (command)
             {
-                int idPers = int.Parse(AskUser("Введите id: "));
-                string namePers = AskUser("Введите name: ");
-                db.Create(idPers, namePers);
-                db.ShowUsers();
-                return;
-            }
-            if (command == "найти")
-            {
-                string name = AskUser("Введите name: ");
-                db.FindByName(name);
-                return;
-            }
-            if (command == "удалить")
-            {
-                db.ShowUsers();
-                int idPers = int.Parse(AskUser("Введите id: "));
-                db.Delete(idPers);
-                db.ShowUsers();
-                return;
-            }
-            if (command == "удалить всех")
-            {
-                db.DeleteAll();
-                return;
-            }
-            if (command == "редактировать")
-            {
-                db.ShowUsers();
-                string name = AskUser("Введите name, кого хотите отредактировать: ");
-                string newName = AskUser("Введите новый name: ");
-                db.Edit(name, newName);
-                db.ShowUsers();
-                return;
+                case Commands.Create:
+                    int idPers = int.Parse(AskUser("Введите id: "));
+                    string namePers = AskUser("Введите name: ");
+                    db.Create(idPers, namePers);
+                    db.ShowUsers();
+                    break;
+                case Commands.Edit:
+                    db.ShowUsers();
+                    string name = AskUser("Введите name, кого хотите отредактировать: ");
+                    string newName = AskUser("Введите новый name: ");
+                    db.Edit(name, newName);
+                    db.ShowUsers();
+                    break;
+                case Commands.Delete:
+                    db.ShowUsers();
+                    int idPerson = int.Parse(AskUser("Введите id: "));
+                    db.Delete(idPerson);
+                    db.ShowUsers();
+                    break;
+                case Commands.DeleteAll:
+                    db.DeleteAll();
+                    break;
+                case Commands.FindByName:
+                    string names = AskUser("Введите name: ");
+                    db.FindByName(names);
+                    break;
             }
 
-            Console.WriteLine("Команда: " + command + " не найдена");
         }
+            
     }
 
     class MyDatabase
